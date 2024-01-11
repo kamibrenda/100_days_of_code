b2b1b6225117f0a9978dc59d9186058c6c3e8c5c
kamibrenda <mukamibrenda254@gmail.com>
2024-01-11 21:24:41 +0300

    Day 4

--- 100 days of Python/day4.py
+++ 100 days of Python/day4.py
@@ -1,3 +1,7 @@
--- # write your SQL statement here: you are given a table 'nospace' with column 'x', return a table with column 'x' and your result in a column named 'res'.
-SELECT x, REPLACE(x, ' ', '') AS RES
-FROM nospace;
+#multiply a number by 8 if its an even number and by 9 if otherwise
+def simple_multiplication(number):
+    if number % 2 == 0:
+        result = number * 8
+    else:
+        result = number * 9
+    return result
